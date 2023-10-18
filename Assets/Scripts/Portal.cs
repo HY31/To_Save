7146a3a41ea62fb7b7b33d0129d211229599b459
kjg5370 <kjk03024@naver.com>
2023-10-18 16:08:54 +0900

    [ADD]SpawnManager
    스폰매니저 생성
    스폰 포인트 지정
    문 오브젝트에 콜라이더 배치
    포탈 스크립트 지정

--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -6,13 +6,17 @@ using UnityEngine.SceneManagement;
 public class Portal : MonoBehaviour
 {
     public string destinationSceneName; // 이동할 대상 씬의 이름
+    public int spawnPointIndex;
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player")) // 플레이어와의 충돌을 감지하면
+        if (other.CompareTag("Player"))
         {
+            Destroy(other.gameObject);
+
             // 대상 씬으로 이동
             SceneManager.LoadScene(destinationSceneName);
+            SpawnManager.Instance.SpawnPlayer(spawnPointIndex);
         }
     }
 }
