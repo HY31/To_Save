abc5495b10889dc4c1003122f82702072a7be57e
kjg5370 <kjk03024@naver.com>
2023-10-19 11:21:15 +0900

    [UPDATE]Portal Script
    포탈 스크립트 수정

--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -7,6 +7,10 @@ public class Portal : MonoBehaviour
 {
     public string destinationSceneName; // 이동할 대상 씬의 이름
     public int spawnPointIndex;
+    private void Start()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -16,6 +20,13 @@ public class Portal : MonoBehaviour
 
             // 대상 씬으로 이동
             SceneManager.LoadScene(destinationSceneName);
+        }
+    }
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        // 모든 씬이 로드될 때 실행할 동작
+        if (scene.isLoaded)
+        {
             SpawnManager.Instance.SpawnPlayer(spawnPointIndex);
         }
     }
