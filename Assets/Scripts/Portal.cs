e30e3974fbd37c637be42111dfac0ce97ac02136
kjg5370 <kjk03024@naver.com>
2023-10-19 14:03:54 +0900

    [UPDATE]Portal
    포탈 스폰인덱스 변수 추가
    스폰매니저 씬로드 이벤트 구독

--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -6,28 +6,14 @@ using UnityEngine.SceneManagement;
 public class Portal : MonoBehaviour
 {
     public string destinationSceneName; // 이동할 대상 씬의 이름
-    public int spawnPointIndex;
-    private void Start()
-    {
-        SceneManager.sceneLoaded += OnSceneLoaded;
-    }
-
+    public int index;
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             Destroy(other.gameObject);
-
-            // 대상 씬으로 이동
+            SpawnManager.Instance.targetSpawnPointIndex = index;
             SceneManager.LoadScene(destinationSceneName);
-        }
-    }
-    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
-    {
-        // 모든 씬이 로드될 때 실행할 동작
-        if (scene.isLoaded)
-        {
-            SpawnManager.Instance.SpawnPlayer(spawnPointIndex);
         }
     }
 }
